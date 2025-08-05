7832c4249105ee9ac51419359ea201bdceb273fa
DanteVela <70284605+DanteVela@users.noreply.github.com>
2025-08-05 18:05:32 -0500

    List Updated

--- frontend/src/ContactList.jsx
+++ frontend/src/ContactList.jsx
@@ -2,6 +2,26 @@
 import React from "react"
 
 const ContactList = ({contacts, updateContact, updateCallback}) => {
+    const onDelete = async (id) => {
+        try {
+            const options = {
+                method: "DELETE"
+            }
+            // Send Delete Request
+            const response = await fetch(`http://127.0.0.1:5000/delete_contact/${id}`, options)
+
+            // If successful, update contact list
+            // Otherwise, catch the error and alert
+            if(response.status === 200) {
+                updateCallback()
+            } else {
+                console.error("Failed to Delete")
+            }
+        } catch (error) {
+            alert(error)
+        }
+    }
+
     return <div>
         <h2>Contacts</h2>
         <table>
@@ -22,7 +42,7 @@ const ContactList = ({contacts, updateContact, updateCallback}) => {
                         <td>{contact.email}</td>
                         <td>
                             <button onClick = {() => updateContact(contact)}>Update</button>
-                            <button>Delete</button>
+                            <button onClick = {() => onDelete(contact.id)}>Delete</button>
                         </td>
                     </tr>
                 ))}
