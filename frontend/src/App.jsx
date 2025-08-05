f6da3f10097e8fa8d05befc70a3d290566778763
DanteVela <70284605+DanteVela@users.noreply.github.com>
2025-08-05 14:13:50 -0500

    Fetch Function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  // Async Function to Fetch Contacts
+  const fetchContacts = async () => {
+    // fetch(): Send a Request [Usually a GET Request]
+    // Flask Server: http://127.0.0.1:5000/contacts
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+
+    // Get the JSON Data
+    const data = await response.json()
+    setContacts(data.contacts)
+    
+    // To see the Data
+    console.log(data.contacts)
+  }
 
   return (
     <>
